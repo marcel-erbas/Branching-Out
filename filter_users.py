316edd22c1aab6e2d66ef01153c0e3348bb527de
Marcel <236373202+marcel-erbas@users.noreply.github.com>
2025-10-19 22:17:17 +0200

    added filter-by-age-feature

--- filter_users.py
+++ filter_users.py
@@ -1,9 +1,15 @@
 import json
 
+USERS_FILE = "users.json"
+
+def load_users():
+    with open(USERS_FILE, "r") as file:
+        users = json.load(file)
+    return users
+
 
 def filter_users_by_name(name):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    users = load_users()
 
     filtered_users = [user for user in users if user["name"].lower() == name.lower()]
 
@@ -11,11 +17,23 @@ def filter_users_by_name(name):
         print(user)
 
 
+def filter_users_by_age(age):
+    users = load_users()
+
+    filtered_users = [user for user in users if str(user["age"]) == age]
+
+    for user in filtered_users:
+        print(user)
+
+
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (Currently, only 'name' and 'age' is supported): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
+    elif filter_option == "age":
+        age_to_search = input("Enter a age to filter users: ").strip()
+        filter_users_by_age(age_to_search)
     else:
         print("Filtering by that option is not yet supported.")
